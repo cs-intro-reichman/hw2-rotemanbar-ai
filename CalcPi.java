702f534f9094853f60a41eba7892611fc593605e
Rotem Anbar <rotemanbar@gmail.com>
2025-11-13 18:50:35 +0200

    Submit HW2

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,14 @@
-// Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		double sum = 0.0;
+		for (int i = 0; i < N; i++) {
+			double sign = ( i % 2 == 0) ? 1.0: -1.0;
+			double term = sign / (2 * i + 1);
+			sum += term;
+		}
+		double approxPi = sum * 4;
+		System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated:     " + approxPi); 
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,14 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+        int totalMinutes = hours * 60 + minutes + minutesToAdd;
+        totalMinutes =  totalMinutes % (24 * 60);
+        int newHours = totalMinutes / 60;
+        int newMinutes = totalMinutes % 60;
+        System.out.printf("%02d:%02d%n", newHours, newMinutes);
+
     }
 }
