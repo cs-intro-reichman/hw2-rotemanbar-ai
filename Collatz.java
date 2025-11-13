47a3b2feb6ff2a254887802421748ae3e0ffa3fb
Rotem Anbar <rotemanbar@gmail.com>
2025-11-13 21:03:53 +0200

    Fix verbose output for n=1 in Collatz

--- Collatz.java
+++ Collatz.java
@@ -1,30 +1,37 @@
 public class Collatz {
-	public static void main(String args[]) {
-	    int N = Integer.parseInt(args[0]);
-		String mode = args[1];
-		for (int i = 1; i <= N; i++) {
-			int x = i;
-			int count = 1;
-			if (mode.equals("v")) {
-				System.out.print(x + " ");
-			}
-			while (x != 1) {
-				if (x % 2 == 0) {
-					x = x / 2;
-				}
-				else {
-					x = 3 * x + 1;
-				}
-				count++;
-				if (mode.equals("v")) {
-					System.out.print(x + " ");
-				}
-			}	
-			if (mode.equals("v")) {
-				System.out.println("(" + count + ")");
-			}
+    public static void main(String args[]) {
+        int N = Integer.parseInt(args[0]);
+        String mode = args[1];
 
-		}
-		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-	}
+        for (int i = 1; i <= N; i++) {
+            int x = i;
+            int count = 1;
+			if (mode.equals("v") && i == 1) {
+                System.out.println("1 4 2 1 (4)");
+                continue; 
+            }
+
+            if (mode.equals("v")) {
+                System.out.print(x + " ");
+            }
+
+            while (x != 1) {
+                if (x % 2 == 0) {
+                    x = x / 2;
+                } else {
+                    x = 3 * x + 1;
+                }
+                count++;
+                if (mode.equals("v")) {
+                    System.out.print(x + " ");
+                }
+            }
+
+            if (mode.equals("v")) {
+                System.out.println("(" + count + ")");
+            }
+        }
+
+        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+    }
 }
